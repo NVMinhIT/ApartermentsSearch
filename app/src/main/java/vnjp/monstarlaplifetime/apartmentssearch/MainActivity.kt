4f61ad3cf69abb6b9a300b30167d3364260d06e8
MinhHoang2003 <hoangd16ptit@gmail.com>
2019-12-27 13:50:38 +0700

    done map

--- app/src/main/java/vnjp/monstarlaplifetime/apartmentssearch/MainActivity.kt
+++ app/src/main/java/vnjp/monstarlaplifetime/apartmentssearch/MainActivity.kt
@@ -1,26 +1,20 @@
 package vnjp.monstarlaplifetime.apartmentssearch
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.stfalcon.pricerangebar.model.BarEntry
-import kotlinx.android.synthetic.main.layout_filter.*
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_filter)
-
-        val barEntrys = ArrayList<BarEntry>()
-
-        barEntrys.add(BarEntry(30.0f, 1.0f))
-        barEntrys.add(BarEntry(32.0f, 7.0f))
-        barEntrys.add(BarEntry(34.0f, 10.0f))
-        barEntrys.add(BarEntry(36.0f, 15.0f))
-        barEntrys.add(BarEntry(38.0f, 7.0f))
-        barEntrys.add(BarEntry(40.0f, 1.0f))
-
-        seekBar.setEntries(barEntrys)
+        setContentView(R.layout.activity_main)
+        mapButton.setOnClickListener {
+            startActivity(Intent(this, MapsActivity::class.java))
+        }
     }
+
 
 }
